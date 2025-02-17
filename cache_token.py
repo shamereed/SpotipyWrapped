9fc1704c17e849fc0a908ad5f2a51282fcd9921a
Shane Reed <shanem.reed@gmail.com>
2025-02-17 15:57:06 -0500

    fixed cache and authorization, added gitignore

--- cache_token.py
+++ cache_token.py
@@ -11,13 +11,24 @@ config_file_path = os.path.join(os.path.dirname(__file__), 'config.cfg')
 config.read(config_file_path)
 
 # Spotify API credentials
-CLIENT_ID = config.get('api_keys', 'client_id')
-CLIENT_SECRET = config.get('api_keys', 'client_secret')
-REDIRECT_URI = config.get('api_keys', 'redirect_uri')
-SCOPE = 'user-top-read'
+CLIENT_ID_CONFIG = config.get('api_keys', 'client_id')
+CLIENT_SECRET_CONFIG = config.get('api_keys', 'client_secret')
+REDIRECT_URI_CONFIG = config.get('api_keys', 'redirect_uri')
+SCOPE_CONFIG = 'user-top-read'
+
+print(CLIENT_ID_CONFIG + ' ' + CLIENT_SECRET_CONFIG + ' ' + REDIRECT_URI_CONFIG)
 
 # Token cache file path
 TOKEN_CACHE_PATH = 'token_cache.json'
+
+sp_oauth = SpotifyOAuth(client_id=CLIENT_ID_CONFIG,
+                        client_secret=CLIENT_SECRET_CONFIG,
+                        redirect_uri=REDIRECT_URI_CONFIG,
+                        scope=SCOPE_CONFIG,
+                        open_browser=True)
+
+print(sp_oauth.get_authorize_url())
+
 
 def load_token_from_cache():
     """Load cached token from the file if it exists and is valid."""
@@ -26,7 +37,7 @@ def load_token_from_cache():
             token_info = json.loads(file.read())
 
             # Check if token is still valid
-            expiration_time = datetime.strptime(token_info['expires_at'],"%Y-%m-%dT%H:%M:%S.%f")
+            expiration_time = datetime.strptime(token_info['expires_at'], "%Y-%m-%dT%H:%M:%S.%f")
             if expiration_time > datetime.now():
                 print("Using cached access token.")
                 return token_info
@@ -49,10 +60,11 @@ def authenticate_spotify():
 
     if token_info is None:
         # If no valid token in cache, request a new token
-        sp_oauth = SpotifyOAuth(client_id=CLIENT_ID,
-                                client_secret=CLIENT_SECRET,
-                                redirect_uri=REDIRECT_URI,
-                                scope=SCOPE)  # Adjust scope based on your needs
+        sp_oauth = SpotifyOAuth(client_id=CLIENT_ID_CONFIG,
+                                client_secret=CLIENT_SECRET_CONFIG,
+                                redirect_uri=REDIRECT_URI_CONFIG,
+                                scope=SCOPE_CONFIG,
+                                open_browser=True)  # Adjust scope based on your needs
 
         # Get the token using authorization code flow
         token_info = sp_oauth.get_access_token(sp_oauth.get_authorize_url())
--- spotipy_requests.py
+++ spotipy_requests.py
@@ -1,29 +1,30 @@
 import spotipy
 from timeit import default_timer as timer
-   
-def getCurrentUserTopItems(sp, type, term) :
+
+
+def getCurrentUserTopItems(sp, type_of_req, term):
     start = timer()
-    results = [];
-    if(type == 'tracks'):
-        results = sp.current_user_top_tracks(limit=25, offset=0,time_range=term+'_term')
+    results = []
+    if type_of_req == 'tracks':
+        results = sp.current_user_top_tracks(limit=25, offset=0, time_range=term + '_term')
     else:
-        results = sp.current_user_top_artists(limit=25, offset=0,time_range=term+'_term')
+        results = sp.current_user_top_artists(limit=25, offset=0, time_range=term + '_term')
     topItems = results['items']
     while results['next']:
         results = sp.next(results)
         topItems.extend(results['items'])
-    
+
     topItemsSorted = []
-    
+
     x = 1
     for item in topItems:
-        if(x<26):
-            if(type == 'tracks'):
+        if x < 26:
+            if type_of_req == 'tracks':
                 topItemsSorted.append(str(x) + ". " + item['name'] + " - " + item['artists'][0]['name'])
             else:
                 topItemsSorted.append(str(x) + ". " + item['name'])
             x += 1
-        
+
     elapsed_time = timer() - start
     print(elapsed_time)
     return topItemsSorted
